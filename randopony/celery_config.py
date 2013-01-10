9cd005660908061f97f80f90b38d87cdc2b3d6e8
Doug Latornell <djl@douglatornell.ca>
2013-01-09 19:23:47 -0800

    Remove celery results & timezone settings; not presently in use.

--- randopony/celery_config.py
+++ randopony/celery_config.py
@@ -8,9 +8,3 @@ BROKER_URL = 'sqla+sqlite:///celery.sqlite'
 CELERY_IMPORTS = (
     'randopony.views.site.populaire',
     )
-
-# CELERY_RESULT_BACKEND = 'database'
-# CELERY_RESULT_DBURI = 'sqlite:///celery.sqlite'
-# CELERY_RESULT_SERIALIZER = 'json'
-
-# CELERY_TIMEZONE = 'Canada/Pacific'
