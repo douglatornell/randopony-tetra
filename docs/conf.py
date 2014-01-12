6fda90ce76f470b5ec7b5f1352725320719d6ca4
Doug Latornell <djl@douglatornell.ca>
2014-01-12 09:59:52 -0800

    Update copyright notice year range.

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'RandoPony'
-copyright = '2012, Doug Latornell'
+copyright = '2012-2014, Doug Latornell'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
