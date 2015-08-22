e26a9dfb16e5bcb91032a4d07dd2131a16c99f4d
Doug Latornell <djl@douglatornell.ca>
2015-08-22 14:07:53 -0700

    Update fabfile production & staging release values re:start of 2015.2 dev.

--- fabfile.py
+++ fabfile.py
@@ -21,10 +21,11 @@ env.user = 'bcrandonneur'
 env.hosts = ['bcrandonneur.webfactional.com']
 project_name = 'randopony-tetra'
 app_name = 'randopony'
-staging_release = '2015r1'
+staging_release = '2015.2'
 staging_dir = (
-    '/home/{0}/webapps/{1}{2}'.format(env.user, app_name, staging_release))
-env.production_release = '2013r3'
+    '/home/{0}/webapps/{1}{2}'
+    .format(env.user, app_name, staging_release.replace('.', '_')))
+env.production_release = '2015r1'
 env.production_dir = (
     '/home/{0}/webapps/{1}{2}'
     .format(env.user, app_name, env.production_release))
